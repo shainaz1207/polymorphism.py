39aa726a3d3b6fd9bb2a289b696145ae20ce0b1f
shainaz1207 <105064443+shainaz1207@users.noreply.github.com>
2022-05-31 23:44:35 +0530

    Create polymorphism.py

---
+++ polymorphism.py
@@ -0,0 +1,18 @@
+class animal:
+    def noise(self):
+       pass
+ class cat(animal):
+     def noise(self):
+         print("meoow")
+ class dog(animal):
+     def noise(self):
+         print("woof")
+ class monkey(animal)
+     def noise(self)
+         print("urrrr")
+ a=cat()
+ a.noise()
+ a=dog()
+ a.noise()
+ a=monkey()
+ a.noise()
